12178a3d03de15343e0d1aac749ae4b45f66a07b
Tells Fox (Silent masonwolf) <silentmason1@gmail.com>
2025-09-01 08:54:03 -0400

    asdf

---
+++ topnav.js
@@ -0,0 +1,7 @@
+const topnavHTML = `
+    <div class="topnav">
+        <a href="index.html">Home</a>
+        <a href="application_form.html">Apply</a>
+        <a href="applications.html">Applications</a>
+    </div>
+`;
